76b88b4749d35fb9936474bc9b063f5beef16e0a
AliElbassuony <alimohamed19499@gmail.com>
2022-03-21 00:07:49 +0200

    Initial commit

--- app.sh
+++ app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+name=$1
+echo "Welocome $name"
 read x y
 
 << comment
@@ -13,14 +15,21 @@ comment
 
 read -t 3 z
 
-n="than"
-q="equal"
+n=than
+q=equal
+un=unseted
+readonly $n
+
+echo before $un
+unset un
 
 # To eval numerical values we use $(()) and put operation between them
 echo $x + $y = $(($x + $y))
+# Another way
+echo `expr $x + $y`
 
 if [ $x -lt $y ]; then
-   printf ""
+   printf "%d less %s %d" $x $n $y
 elif [ $x -eq $y ]; then
    echo $q
 elif [ -z $z ]; then
@@ -28,3 +37,24 @@ elif [ -z $z ]; then
 else
    printf "%d greater $s $d" $x $n $y
 fi
+
+echo $un
+
+# Array Section
+
+arr=("Ali" "Mo" "Ahmed")
+echo ${arr[*]}
+echo ${arr[0]}
+
+i=0
+while [ $i -lt 10 ]; do
+      echo $i
+      i=`expr $i + 1`
+done
+
+j=1
+until [ ! $j -le 10 ]; do
+      echo $j
+      j=`expr $j + 1`
+done
+
