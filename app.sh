a8b8948057a57474b67a3d721f0cf74e2ca601e5
AliElbassuony <alimohamed19499@gmail.com>
2022-03-20 17:08:24 +0200

    Initial commit

--- app.sh
+++ app.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
 read x y
+
+<< comment
+This is multiline commnet
+----read arguments----
+-t => time to leave
+-s => secret password
+-p => message with input
+comment
+
+
 read -t 3 z
 
 n="than"
 q="equal"
 
+# To eval numerical values we use $(()) and put operation between them
 echo $x + $y = $(($x + $y))
 
 if [ $x -lt $y ]; then
@@ -12,7 +24,7 @@ if [ $x -lt $y ]; then
 elif [ $x -eq $y ]; then
    echo $q
 elif [ -z $z ]; then
-	echo "Not Exist"
+	echo "z Not Exist"
 else
    printf "%d greater $s $d" $x $n $y
 fi
