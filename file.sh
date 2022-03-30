1dad6332b25eb702ced8d7ce7feecf39e31c0850
AliElbassuony <alimohamed19499@gmail.com>
2022-03-30 23:41:57 +0200

    Initial commit

---
+++ file.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+#Using for
+
+#for i in $* ;do
+#	echo $*
+#	shift
+#done
+
+# using while
+
+while [ $# -gt 0 ]; do
+	echo $*
+	shift
+done
+
---
+++ pyramid.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+ 
+rows=$1
+for((i=1; i<=rows; i++))
+do   
+	for((j=1; j<=rows - i; j++))
+	do     
+		echo -n "  "   
+	done   
+	for((j=1; j<=2*i - 1; j++))   
+	do     
+		echo -n "* "   
+	done   
+	echo
+done
